562b22647d45d35448c2ef8fdbfd199e76e32839
Tim Mikeladze <tim.mikeladze@gmail.com>
2018-03-31 20:29:38 -0700

    fix(non null types): Use getNamedType

--- packages/graphql-crud/src/util.ts
+++ packages/graphql-crud/src/util.ts
@@ -1,19 +1,14 @@
 import {
   getNamedType,
-  getNullableType,
   GraphQLInputObjectType,
-  GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
 } from 'graphql';
 
 export const toInputObjectTypeName = (name: string): string => `${name}InputType`;
 
-export const isValidInputType = (type, schema: GraphQLSchema): boolean => {
-  if (type instanceof GraphQLList) {
-    return isValidInputType(getNamedType(type), schema);
-  }
-  return !(getNullableType(type) instanceof GraphQLObjectType);
+export const isValidInputFieldType = (type): boolean => {
+  return !(getNamedType(type) instanceof GraphQLObjectType);
 };
 
 export const getInputType = (typeName: string, schema: GraphQLSchema): GraphQLInputObjectType => {
